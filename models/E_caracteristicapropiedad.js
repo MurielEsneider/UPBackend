c7e4293e4d37e2986b848823aa5d73594d83a1d0
Muriels <eesneider2118@gmail.com>
2025-03-21 18:26:59 -0500

    3

--- models/E_caracteristicapropiedad.js
+++ models/E_caracteristicapropiedad.js
@@ -4,114 +4,86 @@ const { Model, DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   class CaracteristicaPropiedad extends Model {
     static associate(models) {
-      CaracteristicaPropiedad.belongsTo(models.Propiedad, {
+      this.belongsTo(models.Propiedad, {
         foreignKey: "propiedad_id",
+        as: 'propiedad',
         onDelete: "CASCADE"
       });
     }
   }
 
   CaracteristicaPropiedad.init({
-    caracteristica_id: {
+    id: {  // Cambiado de caracteristica_id a id
       type: DataTypes.INTEGER,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
+      field: 'caracteristica_id'  // Mapeo a columna existente
     },
     propiedad_id: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      references: {
+        model: 'propiedades',
+        key: 'id'
+      }
     },
-    // Tipo de vivienda (selección única)
     tipo_vivienda: {
       type: DataTypes.ENUM('Apartamento', 'Casa', 'Casa de Familia', 'Estudio', 'Habitación'),
-      allowNull: true
+      allowNull: true,
+      validate: {
+        isIn: {
+          args: [['Apartamento', 'Casa', 'Casa de Familia', 'Estudio', 'Habitación']],
+          msg: "Tipo de vivienda no válido"
+        }
+      }
     },
-    // Servicios (selección múltiple)
-    wifi: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    energia: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    tv: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    cocina: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    agua: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    garaje: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    lavadora: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    nevera: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    gas: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    // Características principales
+    // ... (mantener los demás campos igual pero agregar validaciones)
     habitaciones: {
       type: DataTypes.INTEGER,
-      defaultValue: 1
+      defaultValue: 1,
+      validate: {
+        min: {
+          args: [0],
+          msg: "El número de habitaciones no puede ser negativo"
+        }
+      }
     },
     baños: {
       type: DataTypes.INTEGER,
-      defaultValue: 1
+      defaultValue: 1,
+      validate: {
+        min: {
+          args: [0],
+          msg: "El número de baños no puede ser negativo"
+        }
+      }
     },
-    capacidad: {
-      type: DataTypes.INTEGER,
-      defaultValue: 1
-    },
-    estacionamientos: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0
-    },
-    // Características exteriores
-    jardin: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    piscina: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    vista_montaña: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    terraza: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    // Otros filtros
-    amoblado: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    acepta_mascotas: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    }
+    // ... (agregar validaciones similares para otros campos numéricos)
   }, {
     sequelize,
     modelName: "CaracteristicaPropiedad",
     tableName: 'caracteristicas_propiedades',
     freezeTableName: true,
-    timestamps: true
+    timestamps: true,
+    paranoid: true,  // Habilitar borrado lógico
+    indexes: [
+      {
+        fields: ['propiedad_id'],
+        name: 'idx_propiedad_id'
+      },
+      {
+        fields: ['tipo_vivienda'],
+        name: 'idx_tipo_vivienda'
+      }
+    ],
+    hooks: {
+      beforeValidate: (caracteristica) => {
+        // Normalización de datos
+        if (caracteristica.tipo_vivienda) {
+          caracteristica.tipo_vivienda = caracteristica.tipo_vivienda.trim();
+        }
+      }
+    }
   });
 
   return CaracteristicaPropiedad;
